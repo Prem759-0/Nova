fdb465b17a4790c206ca13dff807d7ba9db3b2aa
prem-web.dev <a70064182@gmail.com>
2026-05-08 22:20:48 +0530

    Create useLenis.ts

---
+++ src/hooks/useLenis.ts
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import Lenis from 'lenis';
+
+export function useLenis() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.4,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      smoothTouch: false,
+      touchMultiplier: 2,
+      wheelMultiplier: 1,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+}
+
+export const LenisProvider = ({ 
+  children 
+}: { 
+  children: React.ReactNode 
+}) => {
+  useLenis();
+  return <>{children}</>;
+};
